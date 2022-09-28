0e52f89f84d9b104eee04d9d4d7281a9b290c455
Tatyana <99961462+TatyanaDaviduk@users.noreply.github.com>
2022-09-28 15:07:11 +0300

    add book

---
+++ scripts/script.js
@@ -0,0 +1,56 @@
+//select
+document.querySelectorAll('.dropdown').forEach(function(dropDownWrapper){
+    const dropDownBtn = dropDownWrapper.querySelector('.dropdown__button');
+    const dropDownList = dropDownWrapper.querySelector('.dropdown__list');
+    const dropDownListItems = dropDownWrapper.querySelectorAll('.dropdown__list-item');
+    const dropDownInput = dropDownWrapper.querySelector('.dropdown__input-hidden');
+
+    dropDownBtn.addEventListener('click', function () {
+        dropDownList.classList.toggle('dropdown__list-visible');
+        this.classList.add('dropdown__button-active');
+    });
+
+    dropDownListItems.forEach(function(listItem){
+        listItem.addEventListener('click',function(e){
+            e.stopPropagation();
+            dropDownBtn.innerText = this.innerText;
+            dropDownList.classList.remove('dropdown__list-visible');
+            dropDownBtn.classList.add('dropdown__button-choose');
+            dropDownInput.value = this.dataset.value;
+        })
+    })
+    //клик снаружи закрываем список
+    document.addEventListener('click', function(e){
+        console.log('document.click');
+        if(e.target !== dropDownBtn){
+            dropDownBtn.classList.remove('dropdown__button-active');
+            dropDownList.classList.remove('dropdown__list-visible');
+        }
+        
+    })
+    
+    //скрыть по нажанию на кнопки
+    document.addEventListener('keydown', function(e){
+        if(e.key === 'Tab' || e.key == 'Escape' ){
+            dropDownBtn.classList.remove('dropdown__button-active');
+            dropDownList.classList.remove('dropdown__list-visible');
+        }
+    })
+})
+
+document.querySelectorAll('.input').forEach(function(input){
+    input.addEventListener('click',function(){
+        this.classList.toggle('input-active');
+    })
+})
+
+//Tab
+
+document.querySelectorAll('.tabs').forEach(function(tabs){
+    const tabsItems = tabs.querySelectorAll('.tabs-nav__item');
+    tabsItems.forEach(item => {
+        item.classList.remove('active');
+        this.classList.add('active');
+        
+    })
+})
